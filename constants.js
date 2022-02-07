54bbfdbeaa44bfaa8e46a032b03548f12cc5f893
Christian Pérez Villanueva <perez.villanueva.christian34@gmail.com>
2022-02-07 15:36:01 -0400

    Added solanart to list of marketplaces

--- constants.js
+++ constants.js
@@ -1,7 +1,9 @@
 export const marketPlaces = {
-    "Magic Eden": ["MEisE1HzehtrDpAAT8PnLHjpSSkRYakotTuJRPjTpo8", "M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K"]
+    "Magic Eden": ["MEisE1HzehtrDpAAT8PnLHjpSSkRYakotTuJRPjTpo8", "M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K"],
+    "Solanart": ["CJsLwbP1iu5DuUikHEJnLfANgKy6stB2uFgvBBHoyxwz"]
 }
 
 export const marketPlaceURLs = {
-    "Magic Eden": "https://www.magiceden.io/item-details"
+    "Magic Eden": "https://www.magiceden.io/item-details",
+    "Solanart": "https://solanart.io/search"
 }
